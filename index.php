a161e5b5237bd57f94472133a0ca3145190dae3c
Thompson Oboyi <105569910+tumazfresh@users.noreply.github.com>
2023-01-31 10:10:25 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Example Project</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="./includes/styles.css" rel="stylesheet">
+  
+</head>
+  <body class="body">
+
+
+  <div class="container text-center">
+  <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1">
+    <div class="col loginbg">
+
+    <div class="row row-cols-1 row-cols-sm-1 row-cols-md-1">
+        <div class="col logintext">
+            LOGIN
+        </div>
+        <form>
+            <div class="col">
+                <input type="text" class="logininput" name="username" placeholder="Username" />
+            </div>
+            <div class="col">
+                <input type="password" class="logininput" name="password" placeholder="****" />
+            </div>
+            <div class="col">
+                <button type="submit" class="loginbtn" name="submit">Login</button>
+            </div>
+        </form>
+
+    </div>
+
+
+    </div>
+    
+  </div>
+</div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
